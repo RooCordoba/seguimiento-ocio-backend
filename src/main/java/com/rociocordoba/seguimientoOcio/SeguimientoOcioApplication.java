8e97632ae257b509f97b6f56d7baa8791588ff91
Roo <roco-fs@hotmail.com>
2024-02-21 16:27:56 -0300

    back base

---
+++ src/main/java/com/rociocordoba/seguimientoOcio/SeguimientoOcioApplication.java
@@ -0,0 +1,13 @@
+package com.rociocordoba.seguimientoOcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguimientoOcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguimientoOcioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rociocordoba/seguimientoOcio/SeguimientoOcioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rociocordoba.seguimientoOcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguimientoOcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
